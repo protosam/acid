e8f158b3114d5b1acd9fe8bbb9d35f9cbadd0b28
Sam <sam.igknighted@gmail.com>
2015-07-17 04:11:41 -0500

    Update connect.php

--- lib/database/connect.php
+++ lib/database/connect.php
@@ -1,17 +1,7 @@
 <?php
-if($CONF['database']['driver'] == 'mysql'){
-	$mysql = mysql_connect($CONF['hostname'], $CONF['database']['username'], $CONF['database']['password'])
-		or fallout("Error connecting to MySQL");
 
-	mysql_select_db($CONF['database']['database'], $mysql) or fallout('Could not select database.');
-
-}else if($CONF['database']['driver'] == 'mysqli'){
-	$mysql = mysqli_connect($CONF['hostname'], $CONF['database']['username'], $CONF['database']['password'], $CONF['database']['database'])
-		or fallout("Error connecting to MySQL");
-}else{
-	fallout("Unknown database driver selected.");
-}
-
+$mysql = mysqli_connect($CONF['hostname'], $CONF['database']['username'], $CONF['database']['password'], $CONF['database']['database'])
+	or fallout("Error connecting to MySQL");
 
 
 function db_close()
