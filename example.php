093629303c4963d5d12b14998496ba202f9e0ecf
Sam <sam.igknighted@gmail.com>
2015-07-19 02:58:40 -0500

    Update and rename index.php to example.php

---
+++ example.php
@@ -0,0 +1,8 @@
+<?php
+/* Welcome to the acid example file!
+ * 
+ * All we have to do to start using acid is require it.
+ */
+require('drop/acid.php');
+
+echo "Welcome! This page is just using the acid framework.";
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require('drop/acid.php');
-
-echo "Hello World";
