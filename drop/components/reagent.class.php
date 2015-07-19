a8906b658893bd115b7221ef589d1a03ab3ea88b
Sam <sam.igknighted@gmail.com>
2015-07-19 17:56:27 -0500

    Update reagent.class.php

--- drop/components/reagent.class.php
+++ drop/components/reagent.class.php
@@ -1,4 +1,19 @@
 <?php
+// we use htmlLawed. Some examples here: http://www.bioinformatics.org/phplabware/internal_utilities/htmLawed/htmLawed_README.htm
+require('drop/3rdparty/htmlLawed.php');
+
+
 class reagent {
-	// sanitization toolkit coming soon
+	
+	public function clean_html($dirty_html){
+		$config = array('safe'=>1); 
+    		return htmLawed($dirty_html, $config);
+	}
+	
+	public function purge_html($dirty_html){
+		$config = array('safe'=>1); 
+    		return htmLawed($dirty_html, $config);
+	}
 }
+
+$reagent = new reagent();
