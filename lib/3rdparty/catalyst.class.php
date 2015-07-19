309b208f32ec076f5eb7aa5b1cf44d4623f7bce9
Sam <sam.igknighted@gmail.com>
2015-07-18 19:11:18 -0500

    Update catalyst.class.php

--- lib/3rdparty/catalyst.class.php
+++ lib/3rdparty/catalyst.class.php
@@ -9,11 +9,13 @@
 	public $link = 'nope';
 	
 	public $primary_key = null;
+	public $table_name = null;
 
 	public $raw_fields = array(); // raw fields with unsanitized data
-	public $mod_fields = array(); // modified fields with sanitized data
+	public $mod_fields = array(); // modified fields with unsanitized data | this is used for efficient row updates
+	
 	public $prepared = array(); // prepared data to replace {variables} with in query statements
-	public $last_query_result = null;
+	public $last_query_result = null; // used by the db $link
 
 	public function __construct(){
 		$this->link = self::$_LINK;
@@ -25,23 +27,76 @@
 	}
 	
 	public function set($var, $val){
-		
+		$this->raw_fields[$var] = $val;
+		$this->mod_fields[$var] = $val;
 	}
 	
 	public function get($var){
 		return $this->raw_fields[$var];
 	}
 	
-	public function findby($var, $val){
+	
+	// fuction for finding results by a single 
+	public function findby($var, $val, $limit = 0, $offset = 0){
+		$this->prepare('value', $val);
+		$sql = "select * from `".$this->table_name."` where $var = '{value}'";
+		if($limit > 0)
+			$sql .= " limit $limit";
+		if($offset > 0)
+			$sql .= " offset $offset";
 		
+		$this->query($sql);
 	}
 	
 	public function save(){
-		
+		// New data needs to be inserted
+		if (!array_key_exists($this->primary_key, $this->raw_fields)) {
+			// INSERT INTO $tablename (col1, col2, col3) VALUES (val1, val2, val3);
+			$final_statement = "INSERT INTO `" . $this->table_name . "` ";
+
+			$fields = '`' . $this->primary_key . '`';
+			$values = 'null';
+			foreach ($this->raw_fields as $field => $value) {
+				$value = $this->link->escape_string($value);
+				$fields .= ', `' . $field . '`';
+			}
+			$final_statement .= "(" . $fields . ") VALUES (" . $values . ");";
+
+			$err_check = $this->link->query($final_statement);
+			if (!$err_check) {
+				echo "MySQL Error: " . $this->link->error . "<br>\n";
+			}
+
+
+			// now we will return only this one
+			$this->findby($this->primary_key, $this->link->insert_id, 1);
+		} else {
+			// UPDATE $tablename SET col1 = val, col2 = val WHERE $primary_key = $this->get($primary_key)
+			$final_statement = "UPDATE `" . $this->table_name . "` SET";
+			$changed_fields = count($this->mod_fields);
+			$builds = 1;
+
+			foreach ($this->mod_fields as $field => $value) {
+				$value = $this->link->escape_string($value);
+				$final_statement .= ' `' . $this->table_name . '`.`' . $field . '` = "' . $value . '"';
+				if ($builds != $changed_fields)
+					$final_statement .= ',';
+				$builds++;
+			}
+
+			$final_statement .= ' WHERE `' . $this->primary_key . '` = "' . $this->raw_fields[$this->primary_key] . '";';
+			$this->link->query($final_statement);
+		}
 	}
 	
 	public function delete(){
-		
+		if ($this->primary_key == null || $this->primary_key == '') {
+			echo 'Can not use delete() method without a set primary_key for ' . $this->table_name;
+			return;
+		}
+		// DELETE FROM table WHERE col = val
+		$final_statement = 'DELETE FROM `' . $this->table_name . '` WHERE `' . $this->primary_key . '` = "' . $this->raw_fields[$this->primary_key] . '";';
+		$this->link->query($final_statement);
 	}
 	
 	// Used to sanitize data going through query()
@@ -88,6 +143,7 @@
 	public function next(){
 		$this->raw_fields = $this->last_query_result->fetch_assoc();
 		$this->mod_fields = array();
+		
 		// return false if we're done providing data
 		if ($this->raw_fields == NULL) {
 			// clean up variables
