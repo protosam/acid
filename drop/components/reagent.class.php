f23c5a41deb1151c30f8c07dad738683d754d2a8
Sam <sam.igknighted@gmail.com>
2015-07-19 20:19:32 -0500

    Update reagent.class.php

--- drop/components/reagent.class.php
+++ drop/components/reagent.class.php
@@ -5,11 +5,6 @@ require('drop/3rdparty/htmlLawed.php');
 
 class reagent {
 
-	public function limit_html($dirty_html, $tags_allowed){
-		$config = array('safe'=>1, 'elements' => 'b, -*');
-    		return htmLawed($dirty_html, $config);
-	}
-
 	public function clean_html($dirty_html){
 		$config = array('safe'=>1);
     		return htmLawed($dirty_html, $config);
@@ -18,6 +13,25 @@ class reagent {
 	public function purge_html($dirty_html){
 		$config = array('safe'=>1, 'elements' => '-*');
     		return htmLawed($dirty_html, $config);
+	}
+	
+	
+	// we use filter_var: http://php.net/manual/en/filter.examples.validation.php
+	// the settings can be found here: http://php.net/manual/en/filter.filters.validate.php
+	public function is_email($email){
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+	
+	public function is_ipaddr($ip){
+		return filter_var($ip, FILTER_VALIDATE_IP);
+	}
+	
+	public function check_length($str, $max){
+		return (strlen($str) > $max);
+	}
+	
+	public function check_length($str, $min, $max){
+		return (strlen($str) > $max || strlen($str) < $min);
 	}
 }
 
