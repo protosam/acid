1d6a90d310ae6d33ffe8861333a2faccc506c9ba
Sam <sam.igknighted@gmail.com>
2015-07-19 19:51:41 -0500

    Update reagent.class.php

--- drop/components/reagent.class.php
+++ drop/components/reagent.class.php
@@ -5,6 +5,11 @@ require('drop/3rdparty/htmlLawed.php');
 
 class reagent {
 
+	public function limit_html($dirty_heml, $tags_allowed){
+		$config = array('safe'=>1, 'elements' => $tags_allowed;
+    		return htmLawed($dirty_html, $config);
+	}
+
 	public function clean_html($dirty_html){
 		$config = array('safe'=>1);
     		return htmLawed($dirty_html, $config);
